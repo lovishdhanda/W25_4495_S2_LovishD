12c7adbaefa9a77127db8fc8101980f33291bdb7
Gurkanwal Singh <111560275+gurkanwall@users.noreply.github.com>
2025-02-10 17:30:36 -0800

    api to get user's own listings

--- Implementation/ecoTots/api/controllers/user.controller.js
+++ Implementation/ecoTots/api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 
 export const test = (req, res) => {
@@ -49,3 +50,18 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const getUserListings = async (req, res, next) => {
+  if(req.user.id === req.param.id){
+    try{
+      const listings = await Listing.find({ userRef: req.param.id});
+      res.status(200).json(listings);
+
+    }catch (error){
+      next(error)
+    }
+  }else{
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+}
