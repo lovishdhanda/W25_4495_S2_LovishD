7de1127ab7f1136cd999c5cedac670a34cea0bb3
Gurkanwal Singh <111560275+gurkanwall@users.noreply.github.com>
2025-03-06 23:12:40 -0800

    send message to seller

--- Implementation/ecoTots/client/src/pages/Listing.jsx
+++ Implementation/ecoTots/client/src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import {
@@ -9,6 +10,7 @@ import {
   FaVenusMars,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
@@ -16,7 +18,9 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
     const params = useParams();
+    const {currentUser} = useSelector((state) => state.user);
     useEffect(() => {
       const fetchListing = async () => {
         try {
@@ -125,6 +129,13 @@ export default function Listing() {
                   Description - 
                     </span>{ listing.description}
                 </p>
+
+                {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                Contact Seller
+              </button>
+            )}
+            {contact && <Contact listing={listing}/>}
               </div>
               </div>
             </div>
